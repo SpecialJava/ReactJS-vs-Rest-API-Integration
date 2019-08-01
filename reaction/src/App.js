e05cca4e2a7bc3fb941ad44d049ebb39f8bbc6ba
javaspecial <shadathcse@gmail.com>
2019-08-01 17:06:40 +0600

    Insert data

--- reaction/src/App.js
+++ reaction/src/App.js
@@ -1,48 +1,96 @@
-  import React, {Component} from 'react';
-  import ReactTable from 'react-table';
-  import 'react-table/react-table.css';
+import React, {Component} from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
-  const API = 'http://192.168.31.21:8080/networld/list_of_user';
+const GET_ALL_USER_API = 'http://192.168.31.21:8080/networld/list_of_user';
+const INSERT_USER_API = 'http://192.168.31.21:8080/networld/add_user';
 
-  export class App extends Component{
+export class App extends Component{
 
-    constructor(props) {
-      super(props); 
-      this.state = {
-        data: [] 
-      }
-    } 
-
-    componentDidMount() {
-     fetch(API).then(response =>{
-      return response.json()
-    }).then(response => {
-     this.setState({ data: response });
-   }).catch(error => {console.log(error); }); 
+  constructor(props) {
+    super(props); 
+    this.state = {
+      data: [] 
+    }
   } 
 
-  render() {
-    const data  = this.state.data.map(response =>{ 
-      return {
-        name: response.userName,
-        email: response.userEmail,
-        note: response.userNote
-      }
-    })
+  componentDidMount() {
+   fetch(GET_ALL_USER_API).then(response =>{
+    return response.json()
+  }).then(response => {
+   this.setState({ data: response });
+ }).catch(error => {console.log(error); }); 
+} 
+
+render() {
+
+  const data  = this.state.data.map(response =>{ 
+    return {
+      id: response.userId,
+      name: response.userName,
+      email: response.userEmail,
+      note: response.userNote
+    }
+  })
 
   const columns = [{
-    Header: 'User Name',
-    accessor: 'name'
-  }, {
-    Header: 'User Email',
-    accessor: 'email',
-  }, {
-   Header: 'User Note',
-   accessor: 'note'
- }]
+   Header: 'User ID',
+   accessor: 'id'
+ },{
+  Header: 'User Name',
+  accessor: 'name'
+}, {
+  Header: 'User Email',
+  accessor: 'email',
+}, {
+ Header: 'User Note',
+ accessor: 'note'
+}]
 
+this.handleChange = (e) =>{
+  this.setState({ name: e.target.value });
+}
 
- return <ReactTable data={data} columns={columns} />
+this.onSubmit = (e) =>{
+  e.preventDefault();
+
+  fetch(INSERT_USER_API, {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    userName:this.state.name,
+    userEmail:this.state.email,
+    userNote:this.state.note,
+    userPassword:this.state.pass
+  })
+})
+
+}
+
+return (
+  <div>
+  <center>
+  <form>
+  <label>Name:<input type="text" name="name" onChange={ this.handleChange } /></label>
+  <label>Email:<input type="text" name="email" value={this.state.email} /></label>
+  <label>Note:<input type="text" name="note" value={this.state.note}/></label>
+  <label>Pass:<input type="text" name="pass" value={this.state.pass}/></label>  
+  <input type="submit" value="Insert Data" onClick={(e) => this.onSubmit(e)}/>
+  </form>
+  <hr/>
+  <label>Enter user ID:<input type="text" /></label>
+  <input type="submit" value="Update"/><br/>
+  
+  <label>Enter email ID:<input type="text" /></label>
+  <input type="submit" value="Delete"/>
+  </center>
+  <hr/>
+  <ReactTable data={data} columns={columns} />
+  </div>
+  );
 }
 
 }
